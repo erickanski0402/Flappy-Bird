9b4a40a0254fcbf7ac03bbb087f97de94d276619
erickanski0402 <erickanski0402@gmail.com>
2019-12-07 12:53:24 -0600

    Reorganize file structure

---
+++ Javascript/script.js
@@ -0,0 +1,44 @@
+var keyState = {};
+window.addEventListener('keydown', function(e){
+  keyState[e.keyCode || e.which] = true;
+}, true);
+window.addEventListener('keyup', function(e){
+  keyState[e.keyCode || e.which] = false;
+}, true);
+
+let radius = 20
+let thickness = 50
+let reset_point = 600 - thickness
+
+function setup(){
+  createCanvas(400,400)
+  player = new player(100, 200, radius)
+
+  pipes = []
+  for(let i = 0; i < 3; i++){
+    pipes[i] = new pipe(reset_point + (i * 200), reset_point, thickness)
+  }
+}
+
+function draw(){
+  background(0,150,200)
+  noStroke()
+
+  if(keyState[32] && player.velY <= 3){
+    player.velY = 5
+  }else{
+    player.velY -= 0.25
+  }
+
+  for(let i = 0; i < 3; i++){
+    pipes[i].movePipe()
+    pipes[i].drawPipe()
+
+    if(pipes[i].checkForCollision(player)){
+      player.posY = 250
+    }
+  }
+
+  player.movePlayer()
+  player.drawPlayer()
+}
